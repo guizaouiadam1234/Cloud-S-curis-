f5669503eb4c86e033d37ebbc81fbafc0f27889f
guizaouiadam1234 <illuminati20043@gmail.com>
2026-01-05 10:51:38 +0100

    first iteration of adding cicd

---
+++ Projet_Cloud_Securise/cicd-manager/src/test/java/com/example/cicd_manager/CicdManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cicd_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
