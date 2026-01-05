c75a11282a5f21e351c6b4eeaa9b33674aa78961
guizaouiadam1234 <illuminati20043@gmail.com>
2026-01-05 15:19:09 +0100

    delete for new project

--- Projet_Cloud_Securise/invocation-api/src/main/java/org/example/invocationapi/repository/InvocableMonsterRepository.java
+++
@@ -1,7 +0,0 @@
-package org.example.invocationapi.repository;
-
-import org.example.invocationapi.model.MonstreInvocable;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface InvocableMonsterRepository extends MongoRepository<MonstreInvocable, String> {
-}
--- Projet_Cloud_Securise/invocation-api/src/main/java/org/example/invocationapi/repository/InvocationRepository.java
+++
@@ -1,9 +0,0 @@
-package org.example.invocationapi.repository;
-
-import org.example.invocationapi.model.Invocation;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import java.util.List;
-
-public interface InvocationRepository extends MongoRepository<Invocation, String> {
-    List<Invocation> findByProcessedFalse();
-}
